a0f550ec13bc3d2f69eed60238f32f5b2c64014f
RodolfoBohn <ensino.rodolfofbohn@gmail.com>
2023-03-06 14:49:26 -0300

    Ajustes versao mobile

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,7 +1,6 @@
 import styles from "./avatar.module.css";
 
 export const Avatar = ({ src, hasBorder = true }) => {
-  console.log("HasBorder", hasBorder);
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
